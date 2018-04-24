61c39fed5318512a410a71594c3f4855255540d5
Davide Depau <davide@depau.eu>
2018-04-24 23:54:33 +0200

    Fix setup.py again

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    DESCRIPTION = f.read()
+
 setup(
     name='DepyTG',
-    version='3.6.post0',
+    version='3.6.0',
     packages=['depytg'],
-    url='https://github.com/Depaulicious/DepyTG',
+    url='https://github.com/Depau/DepyTG',
     author='Davide Depau',
     author_email='davide@depau.eu',
     description='The only Telegram bot library that does nothing',
+    long_description=DESCRIPTION,
+    long_description_content_type='text/markdown',
     license='MIT',
     classifiers=[
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
@@ -21,6 +27,8 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    keywords='telegram bot development',
+    python_requires='>=3',
     requires=["requests"],
     extras_require={
         'flask': ['Flask'],
