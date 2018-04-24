ae3e322b6f0d4c9833acf594cb818126d5ef078c
Davide Depau <davide@depau.eu>
2018-04-25 00:09:16 +0200

    Rename `token` to `url_path` for clarity

--- depytg/webhooks/flask.py
+++ depytg/webhooks/flask.py
@@ -4,18 +4,18 @@ from flask import Flask, Blueprint, request
 from depytg.types import Update
 
 
-def get_app(name: str, token: str, on_update: Callable[[Update], None]):
+def get_app(name: str, url_path: str, on_update: Callable[[Update], None]):
     app = Flask(name)
 
-    get_blueprint(name, token, on_update, app)
+    get_blueprint(name, url_path, on_update, app)
 
 
-def get_blueprint(name: str, token: str, on_update: Callable[[Update], None], bp: Union[Flask, Blueprint] = None):
+def get_blueprint(name: str, url_path: str, on_update: Callable[[Update], None], bp: Union[Flask, Blueprint] = None):
     if not bp:
         bp = Blueprint(name, name)
 
-    @bp.route("/{}".format(token), methods=['POST'])
-    @bp.route("/{}/".format(token), methods=['POST'])
+    @bp.route("/{}".format(url_path), methods=['POST'])
+    @bp.route("/{}/".format(url_path), methods=['POST'])
     def webhook():
         j = request.json()
         on_update(cast(Update, Update.from_json(j)))
