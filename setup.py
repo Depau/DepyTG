116107233f3aebcd29a77aac363a1f0334258fa9
Davide Depau <davide@depau.eu>
2018-04-25 07:11:44 +0200

    Increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name='DepyTG',
-    version='3.6.2.post0',
+    version='3.6.2.post1',
     packages=find_packages(),
     url='https://github.com/Depau/DepyTG',
     author='Davide Depau',
